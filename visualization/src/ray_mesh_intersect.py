37c494a2b20309522d4c33e5805675c47172c29d
Mutiraj Laksanawisit <mutiraj@bu.edu>
2024-07-18 00:34:27 +0700

    Implementing another version of ray mesh intersection for rand_rays

--- visualization/src/ray_mesh_intersect.py
+++ visualization/src/ray_mesh_intersect.py
@@ -15,3 +15,25 @@ def ray_mesh_intersect(R_V, rays, V, F):
     C_V = np.array(C_V)
 
     return C_V, intersected
+
+def ray_mesh_intersect_2(R_V, R_E, V, F):
+    new_R_V = []
+    rays = []
+    for e in R_E:
+        v = R_V[e[1]] - R_V[e[0]]
+        rays.append(v / np.linalg.norm(v))
+        new_R_V.append(R_V[e[0]])
+
+    ts, ids, _ = gpy.ray_mesh_intersect(np.array(new_R_V), np.array(rays), V, F)
+
+    C_V = []
+    intersected = np.array([0] * len(F))
+
+    for i, (t, id) in enumerate(zip(ts, ids)):
+        if id != -1:
+            intersected[id] = 1
+            C_V.append(new_R_V[i] + rays[i] * t)
+
+    C_V = np.array(C_V)
+
+    return C_V, intersected
--- visualization/visualization_main.py
+++ visualization/visualization_main.py
@@ -8,13 +8,14 @@ from src.skeleton_points_sampling import skeleton_points_fixed_length_sampling
 from src.random_ray_vertices import rand_ray
 from src.random_ray_vertices import rand_rays
 from src.ray_mesh_intersect import ray_mesh_intersect
+from src.ray_mesh_intersect import ray_mesh_intersect_2
 
 filename = input()
 V, F = read_mesh(filename)
 S_V, S_E = read_skeleton(filename)
 S_P = skeleton_points_fixed_length_sampling(S_V, S_E, 5e-3)
 vecs = rand_ray(S_V)
-R_V, R_E = rand_rays(S_V, 3, 0.5)
-C_V, intersected = ray_mesh_intersect(S_V, vecs, V, F)
+R_V, R_E = rand_rays(S_P, 3, 1)
+C_V, intersected = ray_mesh_intersect_2(R_V, R_E, V, F)
 
 display_mesh(V, F, C_V = C_V, intersected = intersected, R_V = R_V, R_E = R_E, S_V = S_V, S_E = S_E, S_P = S_P, vecs = vecs, ray_length = 1)
