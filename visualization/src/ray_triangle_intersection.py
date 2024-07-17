28800ef20d5b44db7bd78f9bc50ad35b4e55d8ad
Mutiraj Laksanawisit <mutiraj@bu.edu>
2024-07-17 23:49:25 +0700

    Adding ray_triangle_intersection to visualization_main

---
+++ visualization/src/ray_triangle_intersection.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def ray_triangle_intersection(A, B, C, origin = np.array([0, 0, 0]), ray = np.array([1, 0, 0])):
+    ## Ray
+    e_ = ray/np.linalg.norm(ray)        # Unit vector (versor) of e => ê
+
+    AB = B - A               # Oriented segment A to B
+    AC = C - A               # Oriented segment A to C
+    n = np.cross(AB, AC)     # Normal vector
+    n_ = n/np.linalg.norm(n) # Normalized normal
+
+    # Using the point A to find d
+    d = - np.dot(n_, A)
+
+    # Finding parameter t
+    t = - (np.dot(n_, origin) + d)/np.dot(n_, e_)
+
+    # Finding P
+    P = origin + t*e_
+
+    # Get the resulting vector for each vertex
+    # following the construction order
+    Pa = np.dot(np.cross(B - A, P - A), n_)
+    Pb = np.dot(np.cross(C - B, P - B), n_)
+    Pc = np.dot(np.cross(A - C, P - C), n_)
+
+    if(t < 0):
+        # Means that the triangle has the normal in the opposite direction (same
+        # direction from the ray) or the triangle is behind the ray origin
+        return []
+
+    if (Pa >= 0 and Pb >= 0 and Pc >= 0):
+        return P
+    else:
+        return []
--- visualization/visualization_main.py
+++ visualization/visualization_main.py
@@ -6,6 +6,7 @@ from src.display_mesh import display_mesh
 from src.skeleton_points_sampling import skeleton_points_fixed_length_sampling
 from src.random_ray_vertices import rand_ray
 from src.random_ray_vertices import rand_rays
+from src.ray_triangle_intersection import ray_triangle_intersection
 
 filename = input()
 V, F = read_mesh(filename)
@@ -14,4 +15,14 @@ S_P = skeleton_points_fixed_length_sampling(S_V, S_E, 5e-3)
 vecs = rand_ray(S_V)
 R_V, R_E = rand_rays(S_V, 3, 0.5)
 
-display_mesh(V, F, R_V = R_V, R_E = R_E, S_V = S_V, S_E = S_E, S_P = S_P, vecs = vecs, ray_length = 1)
+C_V = []
+intersected = np.array([0] * len(F))
+for i, vec in enumerate(vecs):
+    for j, f in enumerate(F):
+        intersection_point = ray_triangle_intersection(V[f[0]], V[f[1]], V[f[2]], S_V[i], vec / np.linalg.norm(vec))
+        if len(intersection_point) == 3:
+            intersected[j] = 1
+            C_V.append(intersection_point)
+C_V = np.array(C_V)
+
+display_mesh(V, F, C_V = C_V, intersected = intersected, R_V = R_V, R_E = R_E, S_V = S_V, S_E = S_E, S_P = S_P, vecs = vecs, ray_length = 1)
