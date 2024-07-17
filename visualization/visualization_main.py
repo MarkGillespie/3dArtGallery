04d17afb8b4d5281df05599a874cc1df691b5e41
Mutiraj Laksanawisit <mutiraj@bu.edu>
2024-07-17 22:44:38 +0700

    Define another random rays function which can output more rays per vertices (but the first one looks better)

--- visualization/visualization_main.py
+++ visualization/visualization_main.py
@@ -5,11 +5,13 @@ from src.read_skeleton import read_skeleton
 from src.display_mesh import display_mesh
 from src.skeleton_points_sampling import skeleton_points_fixed_length_sampling
 from src.random_ray_vertices import rand_ray
+from src.random_ray_vertices import rand_rays
 
 filename = input()
 V, F = read_mesh(filename)
 S_V, S_E = read_skeleton(filename)
 S_P = skeleton_points_fixed_length_sampling(S_V, S_E, 5e-3)
 vecs = rand_ray(S_V)
+R_V, R_E = rand_rays(S_V, 3, 0.5)
 
-display_mesh(V, F, S_V = S_V, S_E = S_E, S_P = S_P, vecs = vecs)
+display_mesh(V, F, R_V = R_V, R_E = R_E, S_V = S_V, S_E = S_E, S_P = S_P, vecs = vecs, ray_length = 1)
