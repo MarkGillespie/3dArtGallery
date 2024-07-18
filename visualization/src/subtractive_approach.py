ed2a27942c7a4fd90f8666d43fdbe8db7a06d3e1
Johan-Azambou <89893509+Johan-Azambou@users.noreply.github.com>
2024-07-18 16:56:54 -0500

    added the subtractive approach

---
+++ visualization/src/subtractive_approach.py
@@ -0,0 +1,40 @@
+import numpy as np  
+from src.point_visibility import point_visibility
+
+# get guards using ray_mesh or ray_triangle intersection module
+  
+  
+
+  #check visibility for initial guards
+def subtractive(guards, V, F):
+  guards = []
+  intersected = np.zeros(len(F), dtype=int)
+  visibility = []
+  
+  for p in guards:
+    _, p_intersected = point_visibility(p, V, F)
+    visibility.append(p_intersected)
+    intersected |= p_intersected
+  
+  if not intersected.all():
+    return "missing full coverage"
+    
+  
+  # iteratively remove gaurds with shared visibility
+  i = 0
+  while i < len(guards):
+  
+    # checks what mesh coverage would look like without guard i
+    temp_intersected = np.copy(intersected)
+    temp_intersected &= ~visibility[i]
+  
+    # checks
+    if temp_intersected.all():
+      guards.pop(i)
+      intersected = temp_intersected
+      visibility.pop(i)
+    else: 
+      i += 1
+
+
+  return np.array(guards)
